50bf21c44522a03d3f63f38eab6958d2657a7c1b
Mohammad Taher <zohebtaher@gmail.com>
2021-05-10 14:35:10 -0400

    Added methods to sessions controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Api::SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+        if @user.nil?
+            render json: ['Oops. Wrong email or password']
+        else
+            login!(@user)
+            render 'api/users/show';
+        end
+    end
+
+    def destroy
+        if logged_in?
+            logout!
+            render json: {message:'Logout successful' }
+        else
+            render json: ['You are logged out already'], status: 404
+        end
+    end
+end
