aba9492a0bd4b6571e79060270621b557abf9f20
Mohammad Taher <zohebtaher@gmail.com>
2021-05-28 22:45:23 -0400

    Refactored login to persist user after refresh

--- frontend/splitvilla.jsx
+++ frontend/splitvilla.jsx
@@ -1,36 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {signUp,logIn, logOut ,fetchUsers} from './utils/session_api_util';
-import Root from './components/root';
-import configureStore from './store/store'
+import React from "react";
+import ReactDOM from "react-dom";
+import { signUp, logIn, logOut, fetchUsers } from "./utils/session_api_util";
+import Root from "./components/root";
+import configureStore from "./store/store";
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    let store;
-    
-    if (window.currentUser) {
-        const preloadedState = {
-            entities: {
-                users: { [window.currentUser.user.id]: window.currentUser.user }
-            },
-            session: { id: window.currentUser.id }
-            };
-        store = configureStore(preloadedState);
-            delete window.currentUser;
-        } else {
-        store = configureStore();
-    }
-    
+document.addEventListener("DOMContentLoaded", () => {
+  let store;
 
-    // testing 
-        // window.logIn = logIn;
-        // window.signUp = signUp;
-        // window.logOut = logOut;
-         
-        window.getState = store.getState;
-        window.dispatch = store.dispatch;
-        window.fetchUsers = fetchUsers;
+  if (window.currentUser) {
+    const preloadedState = {
+      session: { id: window.currentUser.user.id },
+      entities: {
+        users: { [window.currentUser.user.id]: window.currentUser.user },
+      },
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
-        
-    const root = document.getElementById("root");
-    ReactDOM.render( <Root store={store}/>, root)
-})
+  // testing
+  // window.logIn = logIn;
+  // window.signUp = signUp;
+  // window.logOut = logOut;
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  window.fetchUsers = fetchUsers;
+
+  const root = document.getElementById("root");
+  ReactDOM.render(<Root store={store} />, root);
+});
